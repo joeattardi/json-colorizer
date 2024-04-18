fd53d9abe625863021829bd4faf63ff4f3e59998
Joe Attardi <jattardi@gmail.com>
2024-04-18 19:53:31 -0400

    Fresh start

--- example.js
+++
@@ -1,9 +0,0 @@
-const colorize = require('./src/lib');
-const pkg = require('./package.json');
-
-console.log(colorize(pkg, {
-  colors: {
-    STRING_LITERAL: '#FF0000'
-  },
-  pretty: true
-}));
--- src/lib/index.js
+++
@@ -1,6 +0,0 @@
-const lexer = require('./lexer');
-const colorizer = require('./colorizer');
-
-module.exports = function colorizeJson(json, options) {
-  return colorizer.colorize(lexer.getTokens(json, options), options);
-};
